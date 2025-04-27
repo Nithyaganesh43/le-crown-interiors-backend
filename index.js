26a2323d67fb75e877d15347b8c409b70e008e6f
Nithya ganesh <nithyaganesh4343@gmail.com>
2025-04-27 23:30:19 +0530

    done

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
 require('dotenv').config();
-
 const imagesRouter = require('./routes/cloudinary');
 const app = express();
 const port = process.env.PORT || 3000;
