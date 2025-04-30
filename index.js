1645350304b67be14a2964c921acb6360d0e626b
Nithya ganesh <nithyaganesh4343@gmail.com>
2025-05-01 00:12:15 +0530

    f

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 const allowedOrigins = ['https://v0-image-management-system.vercel.app'];
 app.use(cors({ origin: allowedOrigins }));
 
-app.use('/image', imageRoute);
+app.use('/', imageRoute);
 
 connectToDb()
   .then(() => {
