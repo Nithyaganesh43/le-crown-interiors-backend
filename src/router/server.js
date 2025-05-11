d4acadb95fe552576e11a8aab6dd3225f30b3977
Nithya ganesh <nithyaganesh4343@gmail.com>
2025-05-11 17:04:21 +0530

    otp

--- src/router/server.js
+++ src/router/server.js
@@ -1,17 +1,28 @@
 require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const serverInit = express.Router();
 const cors = require('cors');
 
-const serverInit = express.Router();
 const allowedOrigins = [
   'https://admin.le-crowninteriors.com',
   'http://127.0.0.1:5500',
 ];
 
+serverInit.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) callback(null, true);
+      else callback(new Error('Not allowed by CORS'));
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
+
 serverInit.use(cookieParser());
 serverInit.use(express.json());
-serverInit.use(cors({ origin: allowedOrigins }));
 
 serverInit.get('/ping', async (req, res) => {
   res.send('Pong from le-crowninteriors Server');
