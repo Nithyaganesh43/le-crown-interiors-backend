87d084ac4a4ac2686ca8cee47efcc6d4cc5d88cc
Nithya ganesh <nithyaganesh4343@gmail.com>
2025-05-11 14:35:15 +0530

    otp

--- src/router/server.js
+++ src/router/server.js
@@ -6,8 +6,8 @@ const cors = require('cors');
 const serverInit = express.Router();
 const allowedOrigins = ['https://admin.le-crowninteriors.com'];
 
-app.use(cookieParser());
-app.use(express.json());
+serverInit.use(cookieParser());
+serverInit.use(express.json());
 serverInit.use(cors({ origin: allowedOrigins }));
 
 serverInit.get('/ping', async (req, res) => {
