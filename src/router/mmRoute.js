9e7fe1519db5faaf8a3486129c6b4b8e8a94ea5c
Nithya ganesh <nithyaganesh4343@gmail.com>
2025-05-12 23:35:04 +0530

    moonogram this and previous

--- src/router/mmRoute.js
+++ src/router/mmRoute.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
 const User = require('../model/user');
 const Post = require('../model/post');
+const multer = require('multer');
+const fs = require('fs');
+const upload = multer({ dest: 'uploads/' });
 
-// create user
 router.post('/signup', async (req, res) => {
   let u = new User({ username: req.body.username });
   await u.save();
   res.send(u);
 });
 
-// follow
 router.post('/follow', async (req, res) => {
   let a = await User.findById(req.body.me);
   let b = await User.findById(req.body.other);
@@ -20,26 +20,28 @@ router.post('/follow', async (req, res) => {
   res.send(b);
 });
 
-// get all posts
 router.get('/posts', async (req, res) => {
   let posts = await Post.find({ published: true }).populate('user', 'username');
   res.send(posts);
 });
 
-// like post
 router.post('/like', async (req, res) => {
   let p = await Post.findById(req.body.postId);
-  if (!p.likes.includes(req.body.userId)) p.likes.push(req.body.userId);
+  let i = p.likes.indexOf(req.body.userId);
+  if (i == -1) p.likes.push(req.body.userId);
+  else p.likes.splice(i, 1);
   await p.save();
   res.send(p);
 });
 
-// create post with schedule
-router.post('/post', async (req, res) => {
+router.post('/post', upload.single('image'), async (req, res) => {
+  let imgPath = req.file.path;
+  let imgData = fs.readFileSync(imgPath).toString('base64');
+  fs.unlinkSync(imgPath);
   let p = new Post({
     user: req.body.userId,
     caption: req.body.caption,
-    image: req.body.image,
+    image: imgData,
     publishAt: new Date(req.body.time),
   });
   await p.save();
@@ -50,7 +52,6 @@ router.post('/post', async (req, res) => {
       let x = await Post.findById(p._id);
       x.published = true;
       await x.save();
-      console.log('Post published:', x.caption);
     }, pub - now);
   } else {
     p.published = true;
@@ -59,11 +60,8 @@ router.post('/post', async (req, res) => {
   res.send(p);
 });
 
-// dummy notify
 router.post('/notify', async (req, res) => {
-  console.log('Notify to:', req.body.userId, req.body.msg);
   res.send({ ok: true });
 });
 
 module.exports = router;
-  
