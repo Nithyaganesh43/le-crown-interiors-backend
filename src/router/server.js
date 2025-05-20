974014404d5c2be6b15686fa2f1bdd9dab3a7a1e
Nithya ganesh <nithyaganesh4343@gmail.com>
2025-05-20 11:19:51 +0530

    otp

--- src/router/server.js
+++ src/router/server.js
@@ -7,12 +7,13 @@ const cors = require('cors');
 const allowedOrigins = [
   'https://admin.le-crowninteriors.com',
   'http://127.0.0.1:5500',
+  'http://localhost:5173',
 ];
 
 serverInit.use(
   cors({
     origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) callback(null, true);
+      if (!origin || allowedOrigins.includes(origin) || true) callback(null, true);
       else callback(new Error('Not allowed by CORS'));
     },
     credentials: true,
