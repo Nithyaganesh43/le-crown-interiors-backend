708bc99585568454f49ac953bac89efba43e76a1
Nithyaganesh43 <154539539+Nithyaganesh43@users.noreply.github.com>
2025-06-14 10:43:39 +0530

    Update server.js

--- src/router/server.js
+++ src/router/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 const allowedOrigins = [
   'https://admin.le-crowninteriors.com',
-  'http://127.0.0.1:5500',
+  'http://127.0.0.1:5500','https://dev.le-crowninteriors.com',
   'http://localhost:5173',
 ];
 
