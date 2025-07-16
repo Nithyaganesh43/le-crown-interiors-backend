82deb680ce9857d45338267dc5371e29ecbf05ce
Nithya ganesh <nithyaganesh4343@gmail.com>
2025-07-16 15:32:27 +0530

    f

--- src/router/server.js
+++ src/router/server.js
@@ -14,12 +14,13 @@ const allowedOrigins = [
   'http://127.0.0.1:5500',
   'http://localhost:5173',
   'http://localhost:3000',
+  'https://gregarious-biscuit-643f8e.netlify.app',
 ];
 
 serverInit.use(
   cors({
     origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) callback(null, true);
+      if (!origin || allowedOrigins.includes(origin) || true ) callback(null, true);
       else callback(new Error('Not allowed by CORS'));
     },
     credentials: true,
